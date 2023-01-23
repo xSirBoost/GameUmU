7a26783967883b16b872622ea541eab6d07cced4
xSirBoost <36288614+xSirBoost@users.noreply.github.com>
2023-01-22 16:42:19 -0800

    Adding Initial

---
+++ settings.py
@@ -0,0 +1,29 @@
+WIDTH    = 1280
+HEIGHT   = 720
+FPS      = 60
+TILESIZE = 64
+
+#ui
+bar_height = 20
+health_bar_width = 200
+energy_bar_width = 140
+item_box_size = 80
+ui_font = './graphics/font/joystix.ttf'
+ui_font_size = 18
+
+water_colour = '#71ddee'
+ui_bg_colour = '#222222'
+ui_border_colour = '#111111'
+text_color= '#EEEEEE'
+
+health_colour = 'red'
+energy_colour = 'blue'
+ui_border_active_colour = 'gold'
+
+weapon_data = {
+    'sword': {'cooldown': 100, 'damage': 15, 'graphics': './graphics/weapons/sword/full.png'},
+    'lance': {'cooldown': 400, 'damage': 30, 'graphics': './graphics/weapons/lance/full.png'},
+    'axe': {'cooldown': 300, 'damage': 20, 'graphics': './graphics/weapons/axe/full.png'},
+    'rapier': {'cooldown': 50, 'damage': 8, 'graphics': './graphics/weapons/rapier/full.png'},
+    'sai': {'cooldown': 80, 'damage': 10, 'graphics': './graphics/weapons/rapier/full.png'},
+}
---
+++ ui.py
@@ -0,0 +1,59 @@
+import pygame 
+from settings import *
+
+class ui:
+    def __init__(self):
+        self.display_surface = pygame.display.get_surface()
+        self.font = pygame.font.Font(ui_font,ui_font_size)
+
+        self.health_bar_rect = pygame.Rect(10, 10, health_bar_width, bar_height)
+        self.energy_bar_rect = pygame.Rect(10, 34, energy_bar_width, bar_height)
+
+        self.weapon_graphics = []
+        for weapon in weapon_data.values():
+            path = weapon['graphics']
+            weapon = pygame.image.load(path).convert_alpha()
+            self.weapon_graphics.append(weapon)
+
+    def show_bar(self, current, max_amount, bg_rect, colour):
+        pygame.draw.rect(self.display_surface, ui_bg_colour, bg_rect)
+
+        ratio = current / max_amount
+        current_width = bg_rect.width * ratio
+        current_rect = bg_rect.copy()
+        current_rect.width = current_width
+
+        pygame.draw.rect(self.display_surface,colour,current_rect)
+        pygame.draw.rect(self.display_surface,ui_border_colour,bg_rect,3)
+
+    def show_exp(self,exp):
+        text_surf = self.font.render(str(int(exp)), False, text_color)
+        x = self.display_surface.get_size()[0] - 20
+        y = self.display_surface.get_size()[1] - 20
+        text_rect = text_surf.get_rect(bottomright = (x, y ))
+        pygame.draw.rect(self.display_surface, ui_bg_colour, text_rect.inflate(20,20))
+        self.display_surface.blit(text_surf, text_rect)
+
+    def weapon_box(self, left, top, has_switched):
+        bg_rect = pygame.Rect(left, top, item_box_size, item_box_size)
+        pygame.draw.rect(self.display_surface,ui_bg_colour,bg_rect)
+        if has_switched:
+            pygame.draw.rect(self.display_surface,ui_border_active_colour,bg_rect, 3)
+        else:
+            pygame.draw.rect(self.display_surface,ui_border_colour,bg_rect, 3)
+        return bg_rect
+
+    def weapon_overlay(self,weapon_index, has_switched):
+        bg_rect = self.weapon_box(10,630,has_switched)
+        weapon_surf = self.weapon_graphics[weapon_index]
+        weapon_rect = weapon_surf.get_rect(center = bg_rect.center)
+        self.display_surface.blit(weapon_surf,weapon_rect)
+
+    def display(self, player):
+        self.show_bar(player.health,player.stats['health'], self.health_bar_rect, health_colour)
+        self.show_bar(player.energy,player.stats['energy'], self.energy_bar_rect, energy_colour)
+
+        self.show_exp(player.exp)
+
+        self.weapon_overlay(player.weapon_index,not player.can_switch_weapon)
+        self.weapon_box(90,630)
